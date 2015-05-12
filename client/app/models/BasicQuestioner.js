582c6a0dfbaf6864314e9931c6b951d503872941
George <gnthackray@btinternet.com>
2015-05-12 22:49:21 +0000

    whatever

--- client/app/models/BasicQuestioner.js
+++ client/app/models/BasicQuestioner.js
@@ -374,7 +374,7 @@ BasicQuestioner.prototype = {
 
                         questionType = (cols[1].indexOf(".jpg") !== -1) ? 2 : questionType;
 
-                        this.questionset.push({ question: cols[questionColIdx], answer: cols[multiAnswerStartIdx], type: questionType, constAnswers: cols[3], score: 0 });
+                        this.questionset.push({ question: cols[questionColIdx], answer: cols[multiAnswerStartIdx], type: questionType, constAnswers: cols[4], score: 0 });
                         this.answerset.push('');
                     }
                 }
