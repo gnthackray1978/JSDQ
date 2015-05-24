d95cce6cc3ea4306ee657507244a203127824e30
George <gnthackray@btinternet.com>
2015-05-24 17:50:32 +0000

    whatever

--- client/app/libs/matchLib.js
+++ client/app/libs/matchLib.js
@@ -32,32 +32,66 @@ MatchLib.prototype.Match =  function (callback) {
 MatchLib.prototype._basicMatch =  function () {
     console.log('basic matching: ' + this.answer + ' == ' + this.solution);
     
-    
-    
-    
-    
     if (this._equals(this.answer,this.solution)) {
             this.matchCallback(true);
         } else {
             this.matchCallback(false);
     }
-        
-        
 };
 
 MatchLib.prototype._equals =  function (answer,solution) {
-     
+     console.log('_equals: ' + answer + ' == ' + solution);
      if(this.isToLower){
         answer = String(answer).toLowerCase();
         solution = String(solution).toLowerCase();
      }
-     
+    
      if ($.trim(answer) == $.trim(solution)) {
             return true;
         } else {
             return false;
     }
 },
+
+MatchLib.prototype._arrayEquals =  function (answer,solution) {
+     
+    var that =this;
+    var answerParts  =answer.split(' ');
+    var solutionParts  =solution.split(' ');
+    
+    var inSolution = function(data){
+        var idx =0;
+        
+        while(idx < solutionParts.length){
+            if(that._equals(solutionParts[idx],data))
+                return true;
+            idx++;
+        }
+        
+        return false;
+    };
+
+    var aIdx =0;
+    var foundCount =0;
+    
+    while(aIdx < answerParts.length){
+        if(inSolution(answerParts[aIdx]))
+            foundCount++;
+        
+        aIdx++;
+    }
+      
+    if(foundCount == solution.length)  {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+      
+},
+
 
 MatchLib.prototype._multiAnswer =  function () {
     console.log('multi answer matching: ' + this.answer + ' == ' + this.solution);
