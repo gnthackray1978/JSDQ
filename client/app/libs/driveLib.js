524deeaeaa8e8b8ab07760ddc4847fd10991bdc3
George <gnthackray@btinternet.com>
2016-03-10 17:08:42 +0000

    refactoring

--- client/app/libs/driveLib.js
+++ client/app/libs/driveLib.js
@@ -3,7 +3,8 @@
 var MyDrive = function (view,channel) {
 
     this.CLIENT_ID = '67881158341-i31rcec2rf6bi26elnf8njnrb7v9ij8q.apps.googleusercontent.com';
-    this.SCOPES = ['https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/plus.login'];
+//this.SCOPES = 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/plus.login';
+    this.SCOPES = 'https://www.googleapis.com/auth/plus.login';
     this.data = null;
     this._channel = channel;
     
