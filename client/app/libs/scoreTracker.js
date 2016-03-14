3223f7e1b225fd0c14c35af47c9c4219ec403697
George <gnthackray@btinternet.com>
2016-03-14 19:38:22 +0000

    refactoring

---
+++ client/app/libs/scoreTracker.js
@@ -0,0 +1,25 @@
+function ScoreTracker(channel) {  
+    
+    this.results = [];
+
+    this.AddNewResult(23,'wcf','c#');
+    this.AddNewResult(75,'wcf','c#');
+    this.AddNewResult(56,'wcf','c#');
+}
+
+ScoreTracker.prototype.AddNewResult = function(score,type,subtype){
+    this.results.push({
+        score: score,
+        type: type,
+        subtype: subtype,
+        date: new Date().toLocaleString()
+    });
+}; 
+
+ScoreTracker.prototype.GetResults = function(type, subType, resultCallback){
+    resultCallback(this.results);
+}; 
+
+ScoreTracker.prototype.ClearResults = function(){
+    this.results = [];
+};
